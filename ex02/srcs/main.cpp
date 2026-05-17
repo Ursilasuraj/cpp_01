954079e52120618a97aa31e14a8d20561ffde262
ursilapradeep <https://github.com/ursilapradeep>
2026-05-17 09:35:30 +0200

    references_to_ptr_&_memoryaddress

---
+++ ex02/srcs/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uvadakku <uvadakku@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/17 08:21:17 by uvadakku          #+#    #+#             */
+/*   Updated: 2026/05/17 09:30:59 by uvadakku         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string mystring = "HI THIS IS BRAIN";
+	std::string *stringPTR = &mystring;
+	std::string &stringREF = mystring;
+	
+	std::cout << " MEMORY ADDRESS " << "\n";
+	std::cout << &mystring << "\n";
+	std::cout << stringPTR << "\n";
+	std::cout << &stringREF << "\n";
+
+	std::cout << "VALUES" << "\n";
+	std::cout << mystring << "\n";
+	std::cout << *stringPTR << "\n";
+	std::cout << stringREF << "\n";
+	
+	return 0;
+}
